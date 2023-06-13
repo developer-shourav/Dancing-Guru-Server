00ec3a51161aafe6fc6a4743a007b1c533cc21e7
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-13 06:39:09 +0600

    primary server setup complete with mongodb intregation

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-console.log('Hello World');
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000 ;
+
+const app = express();
+
+
+
+
+// Middleware
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+  }
+app.use(cors(corsConfig))
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.evuna6q.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      /* await client.connect(); */
+
+      const classCollection = client.db('danceDB').collection('allClass');
+      const usersCollection = client.db('danceDB').collection('users');
+
+      
+
+     
+
+     
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      /* await client.close(); */
+    }
+  }
+  run().catch(console.dir);
+  
+
+
+app.get('/' , (req, res) => {
+    res.send('Welcome to Dancing Guru')
+})
+app.get('/about' , (req, res) => {
+    res.send('Learn About Us')
+})
+
+
+
+app.listen(port , () => {
+    console.log(`Our Dancing Guru is running On the PORT:${port}`);
+})
